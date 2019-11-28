2200770062dca35590471d16120694978b1581d6
David Gahnassia <davidgahnassia@gmail.com>
2019-11-29 01:48:49 +0200

    fix bug

--- client/src/app/panier/panier.component.ts
+++ client/src/app/panier/panier.component.ts
@@ -38,7 +38,7 @@ export class PanierComponent implements OnInit {
   removeItem(model,index){
     let lastProd = JSON.parse(localStorage.getItem('products'));
     Object.keys(lastProd).forEach((key,keyIdx) => {
-      if(keyIdx===index){
+      if(lastProd[key].name === model.name && lastProd[key].type === model.type && keyIdx===index){
         delete lastProd[key];
       }
     })
